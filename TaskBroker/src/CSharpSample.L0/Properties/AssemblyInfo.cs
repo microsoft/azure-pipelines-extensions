121a63c705579b4f4e6f50b4ad5ec6f81e3a8970
Dhashrath <dhgovind@microsoft.com>
2017-06-13 15:08:57 +0530

    Add c# sample project and scripts to build / run tests from ide and cmdline

---
+++ TaskBroker/src/CSharpSample.L0/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSharpSampleTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpSampleTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1d9941bc-a0ae-4acf-a5ff-789ca75d27de")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TaskBroker/src/CSharpSample/CSharpSample.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CSharpSample.cs" company="Microsoft"> 
+// </copyright>
+// <summary>
+//   The c sharp sample.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace CSharpSample
+{
+    /// <summary>
+    /// The c sharp sample.
+    /// </summary>
+    public class CSharpSample
+    {
+        /// <summary>
+        /// The hello.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public static string Hello()
+        {
+            return "Hello, World!";
+        }
+    }
+}
