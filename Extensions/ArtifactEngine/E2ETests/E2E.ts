69da039d93100d4b3169afe526f435ae71966d2e
Gyan Prakash <gykuma@microsoft.com>
2017-11-29 11:53:49 +0530

    add e2e tests (#313)
    * add e2e tests
    resolve cr comments
    
    * add perf tests

--- Extensions/ArtifactEngine/E2ETests/E2E.ts
+++ Extensions/ArtifactEngine/E2ETests/E2E.ts
@@ -8,6 +8,9 @@ import * as providers from "../Providers"
 
 import { BasicCredentialHandler } from "../Providers/handlers/basiccreds";
 import { PersonalAccessTokenCredentialHandler } from "../Providers/handlers/personalaccesstoken";
+import { ArtifactItemStore } from '../Store/artifactItemStore';
+import { TicketState } from '../Models/ticketState';
+import { ItemType } from '../Models/itemType';
 
 var config = require("./config.json")
 
@@ -17,14 +20,13 @@ describe('e2e tests', () => {
         let processor = new engine.ArtifactEngine();
 
         let processorOptions = new engine.ArtifactEngineOptions();
-        processorOptions.fileProcessingTimeoutInMinutes = 5;
         processorOptions.itemPattern = "**";
         processorOptions.parallelProcessingLimit = 8;
-        processorOptions.retryIntervalInSeconds = 1000;
+        processorOptions.retryIntervalInSeconds = 2;
         processorOptions.retryLimit = 2;
         processorOptions.verbose = true;
 
-        var itemsUrl = "http://redvstt-lab43:8080/job/ArtifactEngineJob/6/api/json?tree=artifacts[*]"
+        var itemsUrl = "http://redvstt-lab43:8080/job/ArtifactEngineJob/6/api/json?tree=artifacts[*]";
         var variables = {
             "endpoint": {
                 "url": "http://redvstt-lab43:8080"
@@ -47,11 +49,48 @@ describe('e2e tests', () => {
                     assert.equal(data, "dummyFolderContent");
                     done();
                 });
-                
+
                 assert.equal(tickets.find(x => x.artifactItem.path == "Extensions/ArtifactEngine/TestData/Jenkins/file1.pdb").retryCount, 0);
                 assert.equal(tickets.find(x => x.artifactItem.path == "Extensions/ArtifactEngine/TestData/Jenkins/folder1/file2.txt").retryCount, 0);
             }, (error) => {
-                throw "test failure";
+                throw error;
+            });
+    });
+
+    it('should be able to download build artifact', function (done) {
+        this.timeout(15000);
+        let processor = new engine.ArtifactEngine();
+
+        let processorOptions = new engine.ArtifactEngineOptions();
+        processorOptions.itemPattern = "**";
+        processorOptions.parallelProcessingLimit = 8;
+        processorOptions.retryIntervalInSeconds = 2;
+        processorOptions.retryLimit = 2;
+        processorOptions.verbose = true;
+
+        var itemsUrl = "https://testking123.visualstudio.com/_apis/resources/Containers/1898832?itemPath=Dropz&isShallow=false";
+        var variables = {};
+
+        var handler = new PersonalAccessTokenCredentialHandler(config.vsts.pat);
+        var webProvider = new providers.WebProvider(itemsUrl, "vsts.handlebars", variables, handler, { ignoreSslError: false });
+        var dropLocation = path.join(config.dropLocation, "vstsDropWithMultipleFiles");
+        var filesystemProvider = new providers.FilesystemProvider(dropLocation);
+
+        processor.processItems(webProvider, filesystemProvider, processorOptions)
+            .then((tickets) => {
+                fs.readFile(path.join(config.dropLocation, 'vstsDropWithMultipleFiles/dropz/folder1/file2.txt'), 'utf8', function (err, data) {
+                    if (err) {
+                        throw err;
+                    }
+                    assert.equal(data, "dummyFolderContent");
+                    done();
+                });
+
+                assert.equal(tickets.find(x => x.artifactItem.path == "dropz/file1.pdb").retryCount, 0);
+                assert.equal(tickets.find(x => x.artifactItem.path == "dropz/folder1/file2.txt").retryCount, 0);
+                done();
+            }, (error) => {
+                throw error;
             });
     });
 });
---
+++ Extensions/ArtifactEngine/PerfTests/Perf.ts
@@ -0,0 +1,71 @@
+import * as assert from 'assert';
+import * as path from 'path'
+import * as fs from 'fs'
+
+import * as models from "../Models"
+import * as engine from "../Engine"
+import * as providers from "../Providers"
+
+import { BasicCredentialHandler } from "../Providers/handlers/basiccreds";
+import { PersonalAccessTokenCredentialHandler } from "../Providers/handlers/personalaccesstoken";
+import { ArtifactItemStore } from '../Store/artifactItemStore';
+import { TicketState } from '../Models/ticketState';
+import { ItemType } from '../Models/itemType';
+
+var config = require("./config.json")
+
+describe('perf tests', () => {
+    it('should be able to download large build artifact from vsts drop', function (done) {
+        this.timeout(300000);
+        let processor = new engine.ArtifactEngine();
+
+        let processorOptions = new engine.ArtifactEngineOptions();
+        processorOptions.itemPattern = "**";
+        processorOptions.parallelProcessingLimit = 8;
+        processorOptions.retryIntervalInSeconds = 2;
+        processorOptions.retryLimit = 4;
+        processorOptions.verbose = true;
+
+        var itemsUrl = "https://testking123.visualstudio.com/_apis/resources/Containers/1902716?itemPath=largedrop&isShallow=false";
+        var variables = {};
+
+        var handler = new PersonalAccessTokenCredentialHandler(config.vsts.pat);
+        var webProvider = new providers.WebProvider(itemsUrl, "vsts.handlebars", variables, handler, { ignoreSslError: false });
+        var dropLocation = path.join(config.dropLocation, "vstsDropWithLargeFiles");
+        var filesystemProvider = new providers.FilesystemProvider(dropLocation);
+
+        processor.processItems(webProvider, filesystemProvider, processorOptions)
+            .then((tickets) => {
+                assert.equal(tickets.filter(x => x.artifactItem.itemType === ItemType.File && x.state === TicketState.Processed).length, 301);
+                done();
+            }, (error) => {
+                throw error;
+            });
+    });
+
+    it('should be able to download large build artifact from fileshare', function (done) {
+        this.timeout(900000);
+        let processor = new engine.ArtifactEngine();
+
+        let processorOptions = new engine.ArtifactEngineOptions();
+        processorOptions.itemPattern = "**";
+        processorOptions.parallelProcessingLimit = 8;
+        processorOptions.retryIntervalInSeconds = 2;
+        processorOptions.retryLimit = 4;
+        processorOptions.verbose = true;
+
+        var itemsUrl = "//vscsstor/Users/gykuma/ArtifactEngineTestData/bp/";
+
+        var sourceProvider = new providers.FilesystemProvider(itemsUrl);
+        var dropLocation = path.join(config.dropLocation, "fileshareWithLargeFiles");
+        var filesystemProvider = new providers.FilesystemProvider(dropLocation);
+
+        processor.processItems(sourceProvider, filesystemProvider, processorOptions)
+            .then((tickets) => {
+                assert.equal(tickets.filter(x => x.artifactItem.itemType === ItemType.File && x.state === TicketState.Processed).length, 301);
+                done();
+            }, (error) => {
+                throw error;
+            });
+    });
+});
