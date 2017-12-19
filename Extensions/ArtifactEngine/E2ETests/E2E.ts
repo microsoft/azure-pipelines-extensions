56cf438528f37d699b1dc422950514af5ffd2ab4
Gyan Prakash Kumar <gykuma@microsoft.com>
2017-12-19 15:54:00 +0530

    add tests for huge artifacts
    add e2e for fileshare
    add test for jenkins to perf
    change jenkins server for tests

--- Extensions/ArtifactEngine/E2ETests/E2E.ts
+++ Extensions/ArtifactEngine/E2ETests/E2E.ts
@@ -12,7 +12,7 @@ import { ArtifactItemStore } from '../Store/artifactItemStore';
 import { TicketState } from '../Models/ticketState';
 import { ItemType } from '../Models/itemType';
 
-var config = require("./config.json")
+var config = require("../test.config.json")
 
 describe('e2e tests', () => {
     it('should be able to download jenkins artifact', function (done) {
@@ -26,13 +26,13 @@ describe('e2e tests', () => {
         processorOptions.retryLimit = 2;
         processorOptions.verbose = true;
 
-        var itemsUrl = "http://redvstt-lab43:8080/job/ArtifactEngineJob/6/api/json?tree=artifacts[*]";
+        var itemsUrl = "http://rmcdpjenkins2.southindia.cloudapp.azure.com:8080/job/ReleaseManagement/job/RMCDP/job/ArtifactEngineTests/job/SmallProject/10/api/json?tree=artifacts[*]";
         var variables = {
             "endpoint": {
-                "url": "http://redvstt-lab43:8080"
+                "url": "http://rmcdpjenkins2.southindia.cloudapp.azure.com:8080"
             },
-            "definition": "ArtifactEngineJob",
-            "version": "6"
+            "definition": "ReleaseManagement/job/RMCDP/job/ArtifactEngineTests/job/SmallProject",
+            "version": "10"
         };
 
         var handler = new BasicCredentialHandler(config.jenkins.username, config.jenkins.password);
@@ -57,7 +57,7 @@ describe('e2e tests', () => {
             });
     });
 
-    it('should be able to download build artifact', function (done) {
+    it('should be able to download build artifact from vsts drop', function (done) {
         this.timeout(15000);
         let processor = new engine.ArtifactEngine();
 
@@ -88,7 +88,41 @@ describe('e2e tests', () => {
 
                 assert.equal(tickets.find(x => x.artifactItem.path == "dropz/file1.pdb").retryCount, 0);
                 assert.equal(tickets.find(x => x.artifactItem.path == "dropz/folder1/file2.txt").retryCount, 0);
-                done();
+            }, (error) => {
+                throw error;
+            });
+    });
+
+    it('should be able to download build artifact from fileshare', function (done) {
+        this.timeout(15000);
+        let processor = new engine.ArtifactEngine();
+
+        let processorOptions = new engine.ArtifactEngineOptions();
+        processorOptions.itemPattern = "**";
+        processorOptions.parallelProcessingLimit = 8;
+        processorOptions.retryIntervalInSeconds = 2;
+        processorOptions.retryLimit = 2;
+        processorOptions.verbose = true;
+
+        var itemsUrl = "//vscsstor/Users/gykuma/ArtifactEngineTestData/dropz/";
+        var variables = {};
+
+        var sourceProvider = new providers.FilesystemProvider(itemsUrl);
+        var dropLocation = path.join(config.dropLocation, "fileshareWithMultipleFiles");
+        var destProvider = new providers.FilesystemProvider(dropLocation);
+
+        processor.processItems(sourceProvider, destProvider, processorOptions)
+            .then((tickets) => {
+                fs.readFile(path.join(config.dropLocation, 'fileshareWithMultipleFiles/folder1/file2.txt'), 'utf8', function (err, data) {
+                    if (err) {
+                        throw err;
+                    }
+                    assert.equal(data, "dummyFolderContent");
+                    done();
+                });
+
+                assert.equal(tickets.find(x => x.artifactItem.path == "file1.pdb").retryCount, 0);
+                assert.equal(tickets.find(x => x.artifactItem.path == path.join("folder1","file2.txt")).retryCount, 0);
             }, (error) => {
                 throw error;
             });
--- Extensions/ArtifactEngine/PerfTests/Perf.ts
+++ Extensions/ArtifactEngine/PerfTests/Perf.ts
@@ -13,12 +13,86 @@ import { TicketState } from '../Models/ticketState';
 import { ItemType } from '../Models/itemType';
 import { ArtifactDownloadTicket } from '../Models/artifactDownloadTicket';
 
-var config = require("./config.json")
+var config = require("../test.config.json")
 
 describe('perf tests', () => {
-    //Artifact details => Files: 301, Total Size: 1.7GB
-    it('should be able to download large build artifact from vsts drop', function (done) {
-        this.timeout(300000);
+    //Artifact details => Source: Jenkins, FilesCount: 301, DownloadSize: ~1.7GB, TotalFileSize: ~1.7GB
+    it('should be able to download large size jenkins artifact', function (done) {
+        this.timeout(300000);   //5mins
+        let processor = new engine.ArtifactEngine();
+
+        let processorOptions = new engine.ArtifactEngineOptions();
+        processorOptions.itemPattern = "**";
+        processorOptions.parallelProcessingLimit = 8;
+        processorOptions.retryIntervalInSeconds = 2;
+        processorOptions.retryLimit = 4;
+        processorOptions.verbose = true;
+
+        var itemsUrl = "http://rmcdpjenkins2.southindia.cloudapp.azure.com:8080/job/ReleaseManagement/job/RMCDP/job/ArtifactEngineTests/job/LargeSizeProject/5/api/json?tree=artifacts[*]";
+        var variables = {
+            "endpoint": {
+                "url": "http://rmcdpjenkins2.southindia.cloudapp.azure.com:8080"
+            },
+            "definition": "ReleaseManagement/job/RMCDP/job/ArtifactEngineTests/job/LargeSizeProject",
+            "version": "5"
+        };
+
+        var handler = new BasicCredentialHandler(config.jenkins.username, config.jenkins.password);
+        var webProvider = new providers.WebProvider(itemsUrl, "jenkins.handlebars", variables, handler, { ignoreSslError: false });
+        var dropLocation = path.join(config.dropLocation, "jenkinsDropWithLargeSizeFiles");
+        var filesystemProvider = new providers.FilesystemProvider(dropLocation);
+
+        processor.processItems(webProvider, filesystemProvider, processorOptions)
+            .then((tickets) => {
+                let fileTickets = tickets.filter(x => x.artifactItem.itemType == ItemType.File && x.state === TicketState.Processed);
+                assert.equal(fileTickets.length, 301);
+                assert(getDownloadSizeInMB(fileTickets) > 400);
+                done();
+            }, (error) => {
+                throw error;
+            });
+    });
+
+    //Artifact details => Source: Jenkins, FilesCount: 20377, DownloadSize: ~117MB, TotalFileSize: ~117MB
+    it('should be able to download jenkins artifact with large volume of files', function (done) {
+        this.timeout(1800000);  //30mins
+        let processor = new engine.ArtifactEngine();
+
+        let processorOptions = new engine.ArtifactEngineOptions();
+        processorOptions.itemPattern = "**";
+        processorOptions.parallelProcessingLimit = 8;
+        processorOptions.retryIntervalInSeconds = 2;
+        processorOptions.retryLimit = 4;
+        processorOptions.verbose = true;
+
+        var itemsUrl = "http://rmcdpjenkins2.southindia.cloudapp.azure.com:8080/job/ReleaseManagement/job/RMCDP/job/ArtifactEngineTests/job/LargeVolumeProject/6/api/json?tree=artifacts[*]";
+        var variables = {
+            "endpoint": {
+                "url": "http://rmcdpjenkins2.southindia.cloudapp.azure.com:8080"
+            },
+            "definition": "ReleaseManagement/job/RMCDP/job/ArtifactEngineTests/job/LargeVolumeProject",
+            "version": "6"
+        };
+
+        var handler = new BasicCredentialHandler(config.jenkins.username, config.jenkins.password);
+        var webProvider = new providers.WebProvider(itemsUrl, "jenkins.handlebars", variables, handler, { ignoreSslError: false });
+        var dropLocation = path.join(config.dropLocation, "jenkinsDropWithLargeVolumeFiles");
+        var filesystemProvider = new providers.FilesystemProvider(dropLocation);
+
+        processor.processItems(webProvider, filesystemProvider, processorOptions)
+            .then((tickets) => {
+                let fileTickets = tickets.filter(x => x.artifactItem.itemType == ItemType.File && x.state === TicketState.Processed);
+                assert.equal(fileTickets.length, 20377);
+                assert(getDownloadSizeInMB(fileTickets) > 20);
+                done();
+            }, (error) => {
+                throw error;
+            });
+    });
+
+    //Artifact details => Source: BuildContainer, FilesCount: 301, DownloadSize: ~545MB  TotalFileSize: ~1.7GB
+    it('should be able to download large size build artifact from vsts drop', function (done) {
+        this.timeout(300000);   //5mins
         let processor = new engine.ArtifactEngine();
 
         let processorOptions = new engine.ArtifactEngineOptions();
@@ -40,36 +114,96 @@ describe('perf tests', () => {
             .then((tickets) => {
                 let fileTickets = tickets.filter(x => x.artifactItem.itemType == ItemType.File && x.state === TicketState.Processed);
                 assert.equal(fileTickets.length, 301);
-                assert(getDownloadSizeInMB(fileTickets) > 300);
+                assert(getDownloadSizeInMB(fileTickets) > 400);
                 done();
             }, (error) => {
                 throw error;
             });
     });
 
-    //Artifact details => Files: 301, Total Size: 1.7GB
-    it('should be able to download large build artifact from fileshare', function (done) {
-        this.timeout(900000);
+    //Artifact details => Source: BuildContainer, FilesCount: 20391, DownloadSize: ~23MB, TotalFileSize: ~99MB
+    it('should be able to download build artifact with large volume of files from vsts drop', function (done) {
+        this.timeout(1800000);  //30mins
         let processor = new engine.ArtifactEngine();
 
         let processorOptions = new engine.ArtifactEngineOptions();
         processorOptions.itemPattern = "**";
-        processorOptions.parallelProcessingLimit = 8;
+        processorOptions.parallelProcessingLimit = 64;
         processorOptions.retryIntervalInSeconds = 2;
         processorOptions.retryLimit = 4;
         processorOptions.verbose = true;
 
-        var itemsUrl = "//vscsstor/Users/gykuma/ArtifactEngineTestData/bp/";
+        var itemsUrl = "https://testking123.visualstudio.com/_apis/resources/Containers/1976011?itemPath=files20k&isShallow=false";
+        var variables = {};
+
+        var handler = new PersonalAccessTokenCredentialHandler(config.vsts.pat);
+        var webProvider = new providers.WebProvider(itemsUrl, "vsts.handlebars", variables, handler, { ignoreSslError: false });
+        var dropLocation = path.join(config.dropLocation, "vstsDropWithLargeFiles");
+        var filesystemProvider = new providers.FilesystemProvider(dropLocation);
+
+        processor.processItems(webProvider, filesystemProvider, processorOptions)
+            .then((tickets) => {
+                let fileTickets = tickets.filter(x => x.artifactItem.itemType == ItemType.File && x.state === TicketState.Processed);
+                assert.equal(fileTickets.length, 20391);
+                assert(getDownloadSizeInMB(fileTickets) > 15);
+                done();
+            }, (error) => {
+                throw error;
+            });
+    });
+
+    //Artifact details => Source: FileShare, FilesCount: 301, TotalFileSize: 1.7GB
+    it('should be able to download large size build artifact from fileshare', function (done) {
+        this.timeout(900000);   //15mins
+        let processor = new engine.ArtifactEngine();
+
+        let processorOptions = new engine.ArtifactEngineOptions();
+        processorOptions.itemPattern = "**";
+        processorOptions.parallelProcessingLimit = 64;
+        processorOptions.retryIntervalInSeconds = 2;
+        processorOptions.retryLimit = 4;
+        processorOptions.verbose = true;
+
+        var itemsUrl = "//vscsstor/Users/gykuma/ArtifactEngineTestData/LargeSizeFiles/";
 
         var sourceProvider = new providers.FilesystemProvider(itemsUrl);
-        var dropLocation = path.join(config.dropLocation, "fileshareWithLargeFiles");
+        var dropLocation = path.join(config.dropLocation, "fileshareWithLargeSizeFiles");
         var filesystemProvider = new providers.FilesystemProvider(dropLocation);
 
         processor.processItems(sourceProvider, filesystemProvider, processorOptions)
             .then((tickets) => {
                 let fileTickets = tickets.filter(x => x.artifactItem.itemType == ItemType.File && x.state === TicketState.Processed);
                 assert.equal(fileTickets.length, 301);
-                assert(getDownloadSizeInMB(fileTickets) > 300);
+                assert(getDownloadSizeInMB(fileTickets) > 900);
+                done();
+            }, (error) => {
+                throw error;
+            });
+    });
+
+    //Artifact details => Source: FileShare, FilesCount: 4037, TotalFileSize: ~18MB
+    it('should be able to download build artifact with large volume of files from fileshare', function (done) {
+        this.timeout(3000000);  //50mins
+        let processor = new engine.ArtifactEngine();
+
+        let processorOptions = new engine.ArtifactEngineOptions();
+        processorOptions.itemPattern = "**";
+        processorOptions.parallelProcessingLimit = 64;
+        processorOptions.retryIntervalInSeconds = 2;
+        processorOptions.retryLimit = 4;
+        processorOptions.verbose = true;
+
+        var itemsUrl = "//vscsstor/Users/gykuma/ArtifactEngineTestData/LargeFilesCount/";
+
+        var sourceProvider = new providers.FilesystemProvider(itemsUrl);
+        var dropLocation = path.join(config.dropLocation, "fileshareWithLargeVolumeFiles");
+        var filesystemProvider = new providers.FilesystemProvider(dropLocation);
+
+        processor.processItems(sourceProvider, filesystemProvider, processorOptions)
+            .then((tickets) => {
+                let fileTickets = tickets.filter(x => x.artifactItem.itemType == ItemType.File && x.state === TicketState.Processed);
+                assert.equal(fileTickets.length, 4037);
+                assert(getDownloadSizeInMB(fileTickets) > 15);
                 done();
             }, (error) => {
                 throw error;
@@ -82,5 +216,5 @@ function getDownloadSizeInMB(fileTickets: ArtifactDownloadTicket[]): number {
     for (var ticket of fileTickets) {
         totalDownloadSizeInBytes += ticket.downloadSizeInBytes;
     }
-    return totalDownloadSizeInBytes / (1024 *1024);
+    return totalDownloadSizeInBytes / (1024 * 1024);
 }
