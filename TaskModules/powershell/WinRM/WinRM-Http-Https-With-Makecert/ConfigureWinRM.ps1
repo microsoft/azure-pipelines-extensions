d10d74efd843947a5222f4c04084ff17bd8c1f24
Pavan Adepu <padepu@microsoft.com>
2018-01-02 15:27:57 +0530

    Update ConfigureWinRM.ps1

--- TaskModules/powershell/WinRM/WinRM-Http-Https-With-Makecert/ConfigureWinRM.ps1
+++ TaskModules/powershell/WinRM/WinRM-Http-Https-With-Makecert/ConfigureWinRM.ps1
@@ -86,6 +86,11 @@ function Configure-WinRMHttpsListener
     $thumbprint = (Get-ChildItem cert:\LocalMachine\My | Where-Object { $_.Subject -eq "CN=" + $hostname } | Select-Object -Last 1).Thumbprint
     if(-not $thumbprint)
     {
+        if(-not (Test-Path -Path .\makecert.exe))
+        {
+            throw "File not found: makecert.exe"
+        }
+
         .\makecert -r -pe -n CN=$hostname -b 01/01/2012 -e 01/01/2022 -eku 1.3.6.1.5.5.7.3.1 -ss my -sr localmachine -sky exchange -sp "Microsoft RSA SChannel Cryptographic Provider" -sy 12
         $thumbprint=(Get-ChildItem cert:\Localmachine\my | Where-Object { $_.Subject -eq "CN=" + $hostname } | Select-Object -Last 1).Thumbprint
 
