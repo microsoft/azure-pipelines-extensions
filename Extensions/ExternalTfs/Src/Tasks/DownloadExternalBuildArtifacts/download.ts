1c04e3b38fba3061a2fbc7aad7737cab1edc3ad2
Omesh Pandita <omeshp@microsoft.com>
2018-04-24 15:08:35 +0530

    Add telemetry for download build tasks.

--- Extensions/ExternalTfs/Src/Tasks/DownloadExternalBuildArtifacts/download.ts
+++ Extensions/ExternalTfs/Src/Tasks/DownloadExternalBuildArtifacts/download.ts
@@ -12,6 +12,47 @@ import * as webHandlers from "artifact-engine/Providers/typed-rest-client/Handle
 
 tl.setResourcePath(path.join(__dirname, 'task.json'));
 
+var taskJson = require('./task.json');
+const area: string = 'DownloadExternalBuildArtifacts';
+
+function getDefaultProps() {
+    var hostType = (tl.getVariable('SYSTEM.HOSTTYPE') || "").toLowerCase();
+    return {
+        hostType: hostType,
+        definitionName: '[NonEmail:' + (hostType === 'release' ? tl.getVariable('RELEASE.DEFINITIONNAME') : tl.getVariable('BUILD.DEFINITIONNAME')) + ']',
+        processId: hostType === 'release' ? tl.getVariable('RELEASE.RELEASEID') : tl.getVariable('BUILD.BUILDID'),
+        processUrl: hostType === 'release' ? tl.getVariable('RELEASE.RELEASEWEBURL') : (tl.getVariable('SYSTEM.TEAMFOUNDATIONSERVERURI') + tl.getVariable('SYSTEM.TEAMPROJECT') + '/_build?buildId=' + tl.getVariable('BUILD.BUILDID')),
+        taskDisplayName: tl.getVariable('TASK.DISPLAYNAME'),
+        jobid: tl.getVariable('SYSTEM.JOBID'),
+        agentVersion: tl.getVariable('AGENT.VERSION'),
+        agentOS: tl.getVariable('AGENT.OS'),
+        agentName: tl.getVariable('AGENT.NAME'),
+        version: taskJson.version
+    };
+}
+
+function publishEvent(feature, properties: any): void {
+    try {
+        var splitVersion = (process.env.AGENT_VERSION || '').split('.');
+        var major = parseInt(splitVersion[0] || '0');
+        var minor = parseInt(splitVersion[1] || '0');
+        let telemetry = '';
+        if (major > 2 || (major == 2 && minor >= 120)) {
+            telemetry = `##vso[telemetry.publish area=${area};feature=${feature}]${JSON.stringify(Object.assign(getDefaultProps(), properties))}`;
+        }
+        else {
+            if (feature === 'reliability') {
+                let reliabilityData = properties;
+                telemetry = "##vso[task.logissue type=error;code=" + reliabilityData.issueType + ";agentVersion=" + tl.getVariable('Agent.Version') + ";taskId=" + area + "-" + JSON.stringify(taskJson.version) + ";]" + reliabilityData.errorMessage
+            }
+        }
+        console.log(telemetry);;
+    }
+    catch (err) {
+        tl.warning("Failed to log telemetry, error: " + err);
+    }
+}
+
 async function main(): Promise<void> {
     var promise = new Promise<void>(async (resolve, reject) => {
         var connection = tl.getInput("connection", true);
@@ -111,4 +152,7 @@ async function main(): Promise<void> {
 
 main()
     .then((result) => tl.setResult(tl.TaskResult.Succeeded, ""))
-    .catch((error) => tl.setResult(tl.TaskResult.Failed, error));
+    .catch((err) => {
+        publishEvent('reliability', { issueType: 'error', errorMessage: JSON.stringify(err, Object.getOwnPropertyNames(err)) });
+        tl.setResult(tl.TaskResult.Failed, err)
+    });
