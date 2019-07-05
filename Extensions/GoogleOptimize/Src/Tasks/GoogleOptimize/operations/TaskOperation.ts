94014d3da225c4c535c37ec9e07e0a76317b6f1a
Deepak Dahiya <dahiyadeepak123@gmail.com>
2019-07-05 16:53:50 +0530

    Tab and space error resolved

--- Extensions/GoogleOptimize/Src/Tasks/GoogleOptimize/operations/TaskOperation.ts
+++ Extensions/GoogleOptimize/Src/Tasks/GoogleOptimize/operations/TaskOperation.ts
@@ -4,22 +4,22 @@ import {TaskParameter} from './../models/TaskParameter';
 
 export class TaskOperation {
 
-	private oxclient: Optimizeclient;
-	private param: TaskParameter;
+    private oxclient: Optimizeclient;
+    private param: TaskParameter;
 
-	constructor() {
-		this.oxclient = new Optimizeclient();
-		this.param = TaskParameter.getInstance();
-	}
+    constructor() {
+        this.oxclient = new Optimizeclient();
+        this.param = TaskParameter.getInstance();
+    }
 
-	public async stopExperiment(experiment: IExperiment): Promise < void > {
-		experiment.status = this.param.Status.ENDED;
-		await this.oxclient.updateExperiment(experiment);
-	}
+    public async stopExperiment(experiment: IExperiment): Promise < void > {
+        experiment.status = this.param.Status.ENDED;
+        await this.oxclient.updateExperiment(experiment);
+    }
 
-	public async updateExperiment(experiment: IExperiment): Promise < void > {
-		experiment.status = this.param.Status.RUNNING;
-		await this.oxclient.updateExperiment(experiment);
-	}
+    public async updateExperiment(experiment: IExperiment): Promise < void > {
+        experiment.status = this.param.Status.RUNNING;
+        await this.oxclient.updateExperiment(experiment);
+    }
 
 }
--- Extensions/GoogleOptimize/Src/Tasks/GoogleOptimize/operations/optimizeclient.ts
+++ Extensions/GoogleOptimize/Src/Tasks/GoogleOptimize/operations/optimizeclient.ts
@@ -10,57 +10,57 @@ let url: string;
 
 export class Optimizeclient {
 
-	constructor() {
-		let param = TaskParameter.getInstance();
-		let endpointId = param.endpoint;
-		url = tl.getEndpointUrl(endpointId, true);
-		userAgent = "AzDevOps_GoogleOptimize";
-	}
+    constructor() {
+        let param = TaskParameter.getInstance();
+        let endpointId = param.endpoint;
+        url = tl.getEndpointUrl(endpointId, true);
+        userAgent = "AzDevOps_GoogleOptimize";
+    }
 
-	public async updateExperiment < T extends IExperiment > (experiment: T) {
-		Authorize(async function (err: any, token: any) {
+    public async updateExperiment < T extends IExperiment > (experiment: T) {
+        Authorize(async function (err: any, token: any) {
 
-			let param = TaskParameter.getInstance();
-			if (token == null) {
-				console.log(tl.loc("AccessTokenGenerationFailed", err));
-				tl.setResult(tl.TaskResult.Failed, err);
-			}
+            let param = TaskParameter.getInstance();
+            if (token == null) {
+                console.log(tl.loc("AccessTokenGenerationFailed", err));
+                tl.setResult(tl.TaskResult.Failed, err);
+            }
 
-			const bearerHandler = new hm.BearerCredentialHandler(token);
-			var restclient = new restm.RestClient(userAgent, url, [bearerHandler]);
+            const bearerHandler = new hm.BearerCredentialHandler(token);
+            var restclient = new restm.RestClient(userAgent, url, [bearerHandler]);
 
-			let currentExperiment = await GetExperimentUtil(restclient);
+            let currentExperiment = await GetExperimentUtil(restclient);
 
-			if (currentExperiment.statusCode != 200) {
-				console.log(tl.loc("FailedToFetchCurrentExperiment", JSON.stringify(currentExperiment.error)));
-				tl.setResult(tl.TaskResult.Failed, err);
-				throw tl.loc("FailedToFetchCurrentExperiment", currentExperiment.error);
-			}
+            if (currentExperiment.statusCode != 200) {
+                console.log(tl.loc("FailedToFetchCurrentExperiment", JSON.stringify(currentExperiment.error)));
+                tl.setResult(tl.TaskResult.Failed, err);
+                throw tl.loc("FailedToFetchCurrentExperiment", currentExperiment.error);
+            }
 
-			if (currentExperiment.result.status === param.Status.ENDED) {
-				if (experiment.status === param.Status.ENDED) {
-					console.log(tl.loc("StopFailed"));
-					tl.setResult(tl.TaskResult.Failed, err);
-					throw tl.loc("StopFailed");
-				} else {
-					console.log(tl.loc("UpdateFailed"));
-					tl.setResult(tl.TaskResult.Failed, err);
-					throw tl.loc("UpdateFailed");
-				}
-			}
+            if (currentExperiment.result.status === param.Status.ENDED) {
+                if (experiment.status === param.Status.ENDED) {
+                    console.log(tl.loc("StopFailed"));
+                    tl.setResult(tl.TaskResult.Failed, err);
+                    throw tl.loc("StopFailed");
+                } else {
+                    console.log(tl.loc("UpdateFailed"));
+                    tl.setResult(tl.TaskResult.Failed, err);
+                    throw tl.loc("UpdateFailed");
+                }
+            }
 
-			let restRes = await UpdateExperimentUtil(restclient, experiment);
-			tl.debug(`function: 'updateExperiment'. response: '${JSON.stringify(restRes)}'`);
+            let restRes = await UpdateExperimentUtil(restclient, experiment);
+            tl.debug(`function: 'updateExperiment'. response: '${JSON.stringify(restRes)}'`);
 
-			if (restRes.statusCode != 200) {
-				tl.debug(`Unable to update experiment with Id: '${experiment.id}'. Response:`);
-				tl.debug(JSON.stringify(restRes));
-				console.log(tl.loc("FailedToUpdateExperiment", JSON.stringify(restRes)));
-				tl.setResult(tl.TaskResult.Failed, JSON.stringify(restRes));
-				throw tl.loc("FailedToUpdateExperiment", JSON.stringify(restRes));
-			}
+            if (restRes.statusCode != 200) {
+                tl.debug(`Unable to update experiment with Id: '${experiment.id}'. Response:`);
+                tl.debug(JSON.stringify(restRes));
+                console.log(tl.loc("FailedToUpdateExperiment", JSON.stringify(restRes)));
+                tl.setResult(tl.TaskResult.Failed, JSON.stringify(restRes));
+                throw tl.loc("FailedToUpdateExperiment", JSON.stringify(restRes));
+            }
 
-			console.log(tl.loc("ExperimentWithIdUpdatedSuccessfully", experiment.id));
-		})
-	}
+            console.log(tl.loc("ExperimentWithIdUpdatedSuccessfully", experiment.id));
+        })
+    }
 }
