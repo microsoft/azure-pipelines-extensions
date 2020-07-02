f79793cd91ab324b643978f4665c4d52e00cfd3f
Mark Gardner <mark.gardner@microsoft.com>
2020-07-02 12:04:46 -0400

    always use envvars, even with service connection

--- Extensions/Terraform/Src/Tasks/TerraformTaskV1/src/azure-terraform-command-handler.ts
+++ Extensions/Terraform/Src/Tasks/TerraformTaskV1/src/azure-terraform-command-handler.ts
@@ -15,11 +15,10 @@ export class TerraformCommandHandlerAzureRM extends BaseTerraformCommandHandler 
         this.backendConfig.set('key', tasks.getInput("backendAzureRmKey", true));
         this.backendConfig.set('resource_group_name', tasks.getInput("backendAzureRmResourceGroupName", true));
 
-        //If not set as explicit command line params, the AzureRM provider will search for the backend config as environment vars
-        this.setBackendConfigParameterIfExists(backendServiceName, "subscriptionid", 'arm_subscription_id');
-        this.setBackendConfigAuthorizationParameterIfExists(backendServiceName, "tenantid", 'arm_tenant_id');
-        this.setBackendConfigAuthorizationParameterIfExists(backendServiceName, "serviceprincipalid", 'arm_client_id');
-        this.setBackendConfigAuthorizationParameterIfExists(backendServiceName, "serviceprincipalkey", 'arm_client_secret');
+        process.env['ARM_SUBSCRIPTION_ID']  = tasks.getEndpointDataParameter(backendServiceName, "subscriptionid", false);
+        process.env['ARM_TENANT_ID']        = tasks.getEndpointAuthorizationParameter(backendServiceName, "tenantid", false);
+        process.env['ARM_CLIENT_ID']        = tasks.getEndpointAuthorizationParameter(backendServiceName, "serviceprincipalid", false);
+        process.env['ARM_CLIENT_SECRET']    = tasks.getEndpointAuthorizationParameter(backendServiceName, "serviceprincipalkey", false);
     }
 
     public handleBackend(terraformToolRunner: ToolRunner): void {
