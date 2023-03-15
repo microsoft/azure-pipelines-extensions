0dc1dd9dcd436180b915d004c59a4f4b551bc743
v-mopanda <v-mopanda@microsoft.com>
2023-03-15 14:09:53 +0530

    CG fix for CircleCI

--- Extensions/CircleCI/Src/Tasks/DownloadCircleCIArtifacts/commitsdownloader.ts
+++ Extensions/CircleCI/Src/Tasks/DownloadCircleCIArtifacts/commitsdownloader.ts
@@ -2,7 +2,7 @@ import * as Q from 'q';
 import * as os from 'os';
 import * as path from 'path';
 import * as fs from 'fs';
-import * as tl from 'vsts-task-lib/task';
+import * as tl from 'azure-pipelines-task-lib/task';
 
 import * as providers from 'artifact-engine/Providers';
 import { HttpClientResponse } from 'artifact-engine/Providers/typed-rest-client/HttpClient';
