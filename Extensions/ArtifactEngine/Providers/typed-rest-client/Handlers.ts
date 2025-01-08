df4edaefda51b9cc850be9923ad0c7f60dc1799d
Bhargava Lakshmi Kasu (LTIMINDTREE LIMITED) <v-bkasu@microsoft.com>
2025-01-08 11:42:26 +0530

    changes for ntlm removal code

--- Extensions/ArtifactEngine/Providers/typed-rest-client/Handlers.ts
+++ Extensions/ArtifactEngine/Providers/typed-rest-client/Handlers.ts
@@ -1,4 +1,3 @@
 export { BasicCredentialHandler } from "./handlers/basiccreds";
 export { BearerCredentialHandler } from "./handlers/bearertoken";
-export { NtlmCredentialHandler } from "./handlers/ntlm";
 export { PersonalAccessTokenCredentialHandler } from "./handlers/personalaccesstoken";
