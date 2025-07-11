5d86560bc21503019875b628b82318b381d36b19
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2025-07-11 21:13:45 +0200

    Fix CopyAgentJobVariableToReleaseVariable vulnerabilities

--- Extensions/CopyStageVariableToReleaseVariable/Src/Tasks/CopyAgentJobVariableToReleaseVariable/copyvartorelease.ts
+++ Extensions/CopyStageVariableToReleaseVariable/Src/Tasks/CopyAgentJobVariableToReleaseVariable/copyvartorelease.ts
@@ -1,71 +1,63 @@
-import tl = require('./node_modules/azure-pipelines-task-lib/task');
-const azdev = require('azure-devops-node-api'); 
-import * as ReleaseApi from './node_modules/azure-devops-node-api/ReleaseApi';
-import * as ReleaseInterfaces from './node_modules/azure-devops-node-api/interfaces/ReleaseInterfaces';
+import tl = require('azure-pipelines-task-lib/task');
+const azdev = require('azure-devops-node-api');
+import * as ReleaseApi from 'azure-devops-node-api/ReleaseApi';
+import * as ReleaseInterfaces from 'azure-devops-node-api/interfaces/ReleaseInterfaces';
 
 async function run() {
     try {
-        var stageVariableNamesArray = tl.getDelimitedInput("stageVariableNames", ',', true);
-        var releaseVariableNames = tl.getInput("releaseVariableNames", false);
-        var releaseVariableNamesArray : string[] = [];
-        if (!!releaseVariableNames)
-        {
+        const stageVariableNamesArray = tl.getDelimitedInput("stageVariableNames", ',', true);
+        const releaseVariableNames = tl.getInput("releaseVariableNames", false);
+        let releaseVariableNamesArray: string[] = [];
+
+        if (!!releaseVariableNames) {
             releaseVariableNamesArray = releaseVariableNames.split(',');
-            if(releaseVariableNamesArray.length != stageVariableNamesArray.length)
-            {
+
+            if (releaseVariableNamesArray.length != stageVariableNamesArray.length) {
                 throw new Error(tl.loc("ReleaseVarLengthShoulbeSameAsStageVarLength"));
             }
-        }
-        else
-        {
+        } else {
             tl.debug("Creating release variables with the same name as the stage variables");
             releaseVariableNamesArray = stageVariableNamesArray;
         }
 
-        var collectionUri = tl.getVariable("System.CollectionUri");
-        var teamProject = tl.getVariable("System.TeamProject");
-        var releaseId = tl.getVariable("Release.ReleaseId");
+        const collectionUri = tl.getVariable("System.CollectionUri");
+        const teamProject = tl.getVariable("System.TeamProject")!;
+        const releaseId = Number(tl.getVariable("Release.ReleaseId"))!;
 
-        const accessToken: string = tl.getEndpointAuthorizationParameter('SYSTEMVSSCONNECTION', 'ACCESSTOKEN', false);
-        if (!accessToken)
-        {
+        const accessToken = tl.getEndpointAuthorizationParameter('SYSTEMVSSCONNECTION', 'ACCESSTOKEN', false)!;
+
+        if (!accessToken) {
             throw new Error(tl.loc("JS_AccessTokenNotAvailable"));
         }
 
-        let authHandler = azdev.getHandlerFromToken(accessToken);
-        let connection = new azdev.WebApi(collectionUri, authHandler);    
-        let releaseApiObject: ReleaseApi.IReleaseApi  = await connection.getReleaseApi();
+        const authHandler = azdev.getHandlerFromToken(accessToken);
+        const connection = new azdev.WebApi(collectionUri, authHandler);
+        const releaseApiObject: ReleaseApi.IReleaseApi = await connection.getReleaseApi();
 
         tl.debug("Get release");
-        try
-        {
-            var release: ReleaseInterfaces.Release = await releaseApiObject.getRelease(teamProject, releaseId);
-        }
-        catch(error)
-        {
+
+        let release: ReleaseInterfaces.Release;
+
+        try {
+            release = await releaseApiObject.getRelease(teamProject, releaseId);
+        } catch(error) {
             throw new Error(tl.loc("FailedToFetchRelease", error));
         }
 
-        var variables  = release.variables;
-        for (var i=0; i < stageVariableNamesArray.length; i++)
-        {
-            var configurationVariableValue: ReleaseInterfaces.ConfigurationVariableValue = {};
+        for (let i = 0; i < stageVariableNamesArray.length; i++) {
+            const configurationVariableValue: ReleaseInterfaces.ConfigurationVariableValue = {};
             configurationVariableValue.value = tl.getVariable(stageVariableNamesArray[i]);
             console.log(tl.loc("JS_AssignReleaseVariable", stageVariableNamesArray[i], releaseVariableNamesArray[i]));
-            variables[releaseVariableNamesArray[i]] = configurationVariableValue;
+            release.variables![releaseVariableNamesArray[i]] = configurationVariableValue;
         }
 
-        try
-        {
-            var updatedRelease = await releaseApiObject.updateRelease(release, teamProject, releaseId);
+        try {
+            const updatedRelease = await releaseApiObject.updateRelease(release, teamProject, releaseId);
             console.log(tl.loc("JS_UpdatedRelease", updatedRelease.id));
-        }
-        catch(error)
-        {
+        } catch(error) {
             throw new Error(tl.loc("FailedToUpdateRelease", error));
         }
-    }
-    catch (err) {
+    } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
     }
 }
