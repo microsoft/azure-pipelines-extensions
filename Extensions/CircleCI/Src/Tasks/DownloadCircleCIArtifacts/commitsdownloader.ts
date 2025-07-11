08573991230553caf985ffb122c5cbca890c2bba
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2025-07-11 18:32:47 +0200

    Fix DownloadCircleCIArtifacts vulnerabilities (#1260)
    * Fix DownloadCircleCIArtifacts vulnerabilities
    
    * Move devDependencies to dependencies

--- Extensions/CircleCI/Src/Tasks/DownloadCircleCIArtifacts/commitsdownloader.ts
+++ Extensions/CircleCI/Src/Tasks/DownloadCircleCIArtifacts/commitsdownloader.ts
@@ -1,11 +1,25 @@
-import * as Q from 'q';
+import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
-import * as fs from 'fs';
-import * as tl from 'azure-pipelines-task-lib/task';
 
 import * as providers from 'artifact-engine/Providers';
 import { HttpClientResponse } from 'artifact-engine/Providers/typed-rest-client/HttpClient';
+import * as tl from 'azure-pipelines-task-lib/task';
+import * as Q from 'q';
+
+type BuildObject = {
+    build_num: string;
+    all_commit_details: CommitObject[];
+};
+
+type CommitObject = {
+    id: string;
+    commit: unknown;
+    subject: unknown;
+    author_name: unknown;
+    commit_url: unknown;
+    author_date: unknown;
+};
 
 export class CommitsDownloader {
     private webProvider: providers.WebProvider;
@@ -13,14 +27,14 @@ export class CommitsDownloader {
 
     constructor(webProvider: providers.WebProvider) {
         this.webProvider = webProvider;
-        let retryLimitValue: string = tl.getVariable("VSTS_HTTP_RETRY");
+        let retryLimitValue: string = tl.getVariable("VSTS_HTTP_RETRY")!;
         this.retryLimit = (!!retryLimitValue && !isNaN(parseInt(retryLimitValue))) ? parseInt(retryLimitValue) : 4;
         tl.debug("RetryLimit set to: " + this.retryLimit);
     }
 
     public DownloadFromSingleBuildAndSave(buildId: string): Q.Promise<string> {
         let defer: Q.Deferred<string> = Q.defer<string>();
-        
+
         console.log(tl.loc("GettingCommitsFromSingleBuild", buildId));
         this.GetCommitsFromSingleBuild(buildId).then((commits: string) => {
             this.UploadCommits(commits).then(() => {
@@ -53,20 +67,20 @@ export class CommitsDownloader {
     }
 
     private GetCommitsFromSingleBuild(buildId: string): Q.Promise<string> {
-        let connection = tl.getInput("connection", true);
+        let connection = tl.getInput("connection", true)!;
         let definitionId = tl.getInput("definition", true);
         var endpointUrl = tl.getEndpointUrl(connection, false);
         let defer = Q.defer<string>();
         let url: string = `${endpointUrl}/api/v1.1/project/${definitionId}/${buildId}`;
         url = url.replace(/([^:]\/)\/+/g, "$1");
-        
+
         this.executeWithRetries("getCommitsFromSingleBuild", (url, headers) => { return this.webProvider.webClient.get(url, headers) }, url, { 'Accept': 'application/json' }).then((res: HttpClientResponse) => {
             if (res && res.message.statusCode === 200) {
                 res.readBody().then((body: string) => {
                     let jsonResult = JSON.parse(body);
-                    let commits = [];
+                    let commits: unknown[] = [];
                     if (!!jsonResult) {
-                        jsonResult.all_commit_details.forEach(c => {
+                        jsonResult.all_commit_details.forEach((c: CommitObject) => {
                             let commit = {
                                 "Id": c.commit,
                                 "Message": c.subject,
@@ -76,7 +90,7 @@ export class CommitsDownloader {
                                 "DisplayUri": c.commit_url,
                                 "Timestamp": c.author_date
                             };
-    
+
                             commits.push(commit);
                         });
                     }
@@ -95,15 +109,15 @@ export class CommitsDownloader {
     }
 
     private GetCommits(startBuildId: string, endBuildId: string): Q.Promise<string> {
-        let connection = tl.getInput("connection", true);
+        let connection = tl.getInput("connection", true)!;
         let definitionId = tl.getInput("definition", true);
         var endpointUrl = tl.getEndpointUrl(connection, false);
         let defer = Q.defer<string>();
         let url: string = `${endpointUrl}/api/v1.1/project/${definitionId}/${startBuildId}`;
         url = url.replace(/([^:]\/)\/+/g, "$1");
-        let commits = [];
-        
-        this.executeWithRetries("getCommits", (url, headers) => { return this.webProvider.webClient.get(url, headers) }, url, { 'Accept': 'application/json' }).then((res: HttpClientResponse) => {
+        let commits: unknown[] = [];
+
+        this.executeWithRetries("getCommits", (url: string, headers: unknown) => { return this.webProvider.webClient.get(url, headers) }, url, { 'Accept': 'application/json' }).then((res: HttpClientResponse) => {
             if (res && res.message.statusCode === 200) {
                 res.readBody().then((body: string) => {
                     let jsonResult = JSON.parse(body);
@@ -121,11 +135,11 @@ export class CommitsDownloader {
                     this.webProvider.webClient.get(buildsUrl, { 'Accept': 'application/json' }).then((res: HttpClientResponse) => {
                         res.readBody().then((body: string) => {
                             let builds = JSON.parse(body);
-                            let commitsIdsMap = {};
+                            let commitsIdsMap: Record<string, unknown> = {};
                             if (!!builds) {
-                                builds.forEach(build => {
+                                builds.forEach((build: BuildObject) => {
                                     if (Number(build.build_num) <= Number(endBuildId) && Number(build.build_num) >= Number(startBuildId) && build.all_commit_details[0]) {
-                                        build.all_commit_details.forEach(c => {
+                                        build.all_commit_details.forEach((c: CommitObject) => {
                                             let commit = {
                                                 "Id": c.commit,
                                                 "Message": c.subject,
@@ -135,16 +149,16 @@ export class CommitsDownloader {
                                                 "DisplayUri": c.commit_url,
                                                 "Timestamp": c.author_date
                                             };
-        
-                                            if (!commitsIdsMap[commit.Id]) {
+
+                                            if (!commitsIdsMap[commit.Id as string]) {
                                                 commits.push(commit);
-                                                commitsIdsMap[commit.Id] = true;
+                                                commitsIdsMap[commit.Id as string] = true;
                                             }
                                         });
                                     }
                                 });
                             }
-                            
+
                             tl.debug("Downloaded " + commits.length + " commits");
                             defer.resolve(JSON.stringify(commits));
                         }, (error) => {
@@ -168,6 +182,7 @@ export class CommitsDownloader {
         console.log(tl.loc("WritingCommitsTo", commitsFilePath));
         this.WriteContentToFileAndUploadAsAttachment(commits, commitsFilePath).then(() => {
             console.log(tl.loc("SuccessfullyUploadedCommitsAttachment"));
+            // @ts-ignore
             defer.resolve(null);
         }, (error) => {
             defer.reject(error);
@@ -178,7 +193,7 @@ export class CommitsDownloader {
 
     private GetCommitsFileName(): string {
         let fileName: string = "commits.json";
-        let commitfileName: string = tl.getInput("artifactDetailsFileNameSuffix", false);
+        let commitfileName: string = tl.getInput("artifactDetailsFileNameSuffix", false)!;
 
         if (commitfileName) {
             fileName = `commits_${commitfileName}`;
@@ -204,21 +219,22 @@ export class CommitsDownloader {
 
             console.log(tl.loc("UploadingAttachment", filePath));
             console.log(`##vso[task.uploadfile]${filePath}`);
+            // @ts-ignore
             defer.resolve(null);
         });
 
         return defer.promise;
     }
 
-    private executeWithRetries(operationName: string, operation: (string, any) => Promise<any>, url, headers): Promise<any> {
-        var executePromise = new Promise((resolve, reject) => {
+    private executeWithRetries(operationName: string, operation: (url: string, headers: unknown) => Promise<any>, url: string, headers: unknown): Promise<HttpClientResponse> {
+        var executePromise = new Promise<HttpClientResponse>((resolve, reject) => {
             this.executeWithRetriesImplementation(operationName, operation, this.retryLimit, url, headers, resolve, reject);
         });
-    
+
         return executePromise;
     }
-    
-    private executeWithRetriesImplementation(operationName: string, operation: (string, any) => Promise<any>, currentRetryCount, url, headers, resolve, reject) {
+
+    private executeWithRetriesImplementation(operationName: string, operation: (string: string, headers: unknown) => Promise<any>, currentRetryCount: number, url: string, headers: unknown, resolve: (v: HttpClientResponse) => void, reject: (reason: unknown) => void) {
         operation(url, headers).then((result) => {
             resolve(result);
         }).catch((error) => {
