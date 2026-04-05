f7ec384239dd85a868fd9ec416d05ace8a706db9
Gayatri Jaiswal (MINDTREE LIMITED) <v-gayjaiswal@microsoft.com>
2026-04-05 12:06:45 +0530

    fix npm config

--- gulpfile.js
+++ gulpfile.js
@@ -666,7 +666,7 @@ function runNpmInstall(packagePath) {
     util.cd(packagePath);
     var packageJsonPath = util.rp('package.json');
     if (util.test('-f', packageJsonPath)) {
-        util.run(`npm install --userconfig ${path.join(artifactEngineSourcePath, ".npmrc")}`);
+        util.run(`npm install --userconfig ${path.resolve(__dirname, ".npmrc")}`);
     }
     util.cd(originalDir);
 }
