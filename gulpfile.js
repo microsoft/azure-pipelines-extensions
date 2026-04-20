825b4d411beee76a254fb2797fa876df24d04062
Gayatri Jaiswal (MINDTREE LIMITED) <v-gayjaiswal@microsoft.com>
2026-04-21 04:51:48 +0530

    resolve .npmrc path relative to repo

--- gulpfile.js
+++ gulpfile.js
@@ -669,7 +669,7 @@ function runNpmInstall(packagePath) {
     util.cd(packagePath);
     var packageJsonPath = util.rp('package.json');
     if (util.test('-f', packageJsonPath)) {
-        util.run(`npm install --userconfig ${path.resolve(artifactEngineSourcePath, ".npmrc")}`);
+        util.run(`npm install --userconfig ${path.resolve(__dirname, artifactEngineSourcePath, ".npmrc")}`);
     }
     util.cd(originalDir);
 }
