08ed4897fdbc454855e5d45703d34e812ca42e32
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2026-04-27 17:18:59 +0200

    Remove Q dependency and refactor promise handling in task validation (#1365)
    * Remove Q dependency and refactor promise handling in task validation
    
    * Fix error handling in copyCommonModules for task.json existence check

--- package.js
+++ package.js
@@ -4,14 +4,19 @@ const path = require('path');
 const fs = require('fs-extra');
 const gulp = require('gulp');
 const gutil = require('gulp-util');
-const Q = require('q');
 const shell = require('shelljs');
 const through = require('through2');
 const check = require('validator');
 
 const util = require('./package-utils');
+const TEMP_PATH = path.join(__dirname, '_temp');
 
-var createError = function(msg) {
+/**
+ * Creates a new PluginError with the specified message for the 'PackageTask' plugin.
+ * @param {string} msg - The error message to include in the PluginError.
+ * @returns {gutil.PluginError} A new PluginError instance with the provided message.
+ */
+function createError(msg) {
     return new gutil.PluginError('PackageTask', msg);
 }
 
@@ -31,34 +36,35 @@ var createError = function(msg) {
  * @property {TaskExecutor} execution - The execution type of the task, e.g., 'Node'.
  */
 
-// Validates the structure of a task.json file.
-var validateTask = function(folderName, task) {
-    var defer = Q.defer();
+/**
+ * Validates the structure of a task.json file.
+ * @param {string} folderName - The name of the folder containing the task, used for error messages if the task name is missing or invalid.
+ * @param {Task} task - The task object parsed from task.json.
+ * @returns {Promise<void>}
+ */
+function validateTask(folderName, task) {
+    return new Promise((resolve, reject) => {
+        const vn = (task.name || folderName);
 
-    var vn = (task.name || folderName);
+        if (!task.id || !check.isUUID(task.id)) {
+            return reject(createError(`${vn}: id is a required guid`));
+        } else if (!task.name || !check.isAlphanumeric(task.name)) {
+            return reject(createError(`${vn}: name is a required alphanumeric string`));
+        } else if (!task.friendlyName || !check.isLength(task.friendlyName, 1, 40)) {
+            return reject(createError(`${vn}: friendlyName is a required string <= 40 chars`));
+        } else if (!task.instanceNameFormat) {
+            return reject(createError(`${vn}: instanceNameFormat is required`));
+        }
 
-    if (!task.id || !check.isUUID(task.id)) {
-        defer.reject(createError(vn + ': id is a required guid'));
-    };
-
-    if (!task.name || !check.isAlphanumeric(task.name)) {
-        defer.reject(createError(vn + ': name is a required alphanumeric string'));
-    }
-
-    if (!task.friendlyName || !check.isLength(task.friendlyName, 1, 40)) {
-        defer.reject(createError(vn + ': friendlyName is a required string <= 40 chars'));
-    }
-
-    if (!task.instanceNameFormat) {
-        defer.reject(createError(vn + ': instanceNameFormat is required'));
-    }
-
-    // resolve if not already rejected
-    defer.resolve();
-    return defer.promise;
+        return resolve();
+    });
 };
-var _tempPath = path.join(__dirname, '_temp');
 
+/**
+ * Parses the contents of a task.json file into a Task object.
+ * @param {string} jsonContents
+ * @returns {Task|null} The parsed Task object if successful, or null if parsing fails.
+ */
 function parseTaskJson(jsonContents) {
     try {
         return JSON.parse(jsonContents);
@@ -76,114 +82,111 @@ function parseTaskJson(jsonContents) {
  * @returns {NodeJS.ReadWriteStream} A through2 stream that processes task.json files, copies common modules, and builds UI contributions.
  */
 function copyCommonModules(currentExtnRoot, commonDeps, commonSrc, extensionSourcePath) {
-    return through.obj(
-        function(taskJson, _encoding, done) {
-            if (!fs.existsSync(taskJson)) {
-                new gutil.PluginError('PackageTask', 'Task json cannot be found: ' + taskJson.path);
+    return through.obj(function(taskJson, _encoding, done) {
+        if (!fs.existsSync(taskJson.path)) {
+            return done(createError('Task json cannot be found: ' + taskJson.path));
+        }
+
+        if (taskJson.isNull() || taskJson.isDirectory()) {
+            this.push(taskJson);
+            return done();
+        }
+
+        const taskDirPath = path.dirname(taskJson.path);
+        const folderName = path.basename(taskDirPath);
+
+        /** @type {Task|null} */
+        const task = parseTaskJson(taskJson.contents.toString());
+
+        if (task === null) {
+            done(createError('Parse error in task.json: ' + taskJson.path));
+            return;
+        }
+
+        validateTask(folderName, task)
+        .then(function () {
+            // Copy the task to the layout folder.
+            const targetPath = path.join(currentExtnRoot, "Src", "Tasks", task.name);
+            const taskSourcePath = path.join(extensionSourcePath, "Src", "Tasks", task.name);
+            shell.mkdir('-p', targetPath);
+            shell.rm('-f', path.join(targetPath, '*.csproj'));
+            shell.rm('-f', path.join(targetPath, '*.md'));
+            // Path to UI contribution files
+            const uiPath = path.join(currentExtnRoot, "Src", "UIContribution");
+            // Statically link the required internal common modules.
+            let taskDeps;
+
+            if ((taskDeps = commonDeps[folderName])) {
+                taskDeps.forEach(function (dep) {
+                    gutil.log('Linking ' + dep.module + ' into ' + folderName);
+                    const src = path.join(commonSrc, dep.module, "Src/");
+                    const dest = path.join(targetPath, dep.dest);
+                    shell.mkdir('-p', dest);
+                    fs.copy(src, dest, function (err) {
+                        if (err) return console.error(err)
+                    });
+                })
             }
 
-            if (taskJson.isNull() || taskJson.isDirectory()) {
-                this.push(taskJson);
-                return done();
+            const externals = require('./externals.json');
+
+            // For building UI contribution using webpack
+            if (fs.existsSync(uiPath) && fs.statSync(uiPath).isDirectory()) {
+                console.log(`⚒️  Building UI contribution for task: ${task.name}`);
+                const originalDir = shell.pwd();
+                util.cd(uiPath);
+                util.run('npm ci');
+                util.cd(originalDir);
             }
 
-            var taskDirPath = path.dirname(taskJson.path);
-            var folderName = path.basename(taskDirPath);
+            if (Object.keys(task.execution).some(x => x.includes('Node'))) {
+                if (externals['no-cache'].includes(task.name)) {
+                    console.log(`⚒️  Building Node task: ${task.name}`);
 
-            /** @type {Task} */
-            var task = parseTaskJson(taskJson.contents.toString());
+                    const originalDir = shell.pwd();
 
-            if (task === null) {
-                done(createError('Parse error in task.json: ' + taskJson.path));
-                return;
-            }
-
-
-            validateTask(folderName, task)
-            .then(function() {
-                // Copy the task to the layout folder.
-                const targetPath = path.join(currentExtnRoot, "Src", "Tasks", task.name);
-                const taskSourcePath = path.join(extensionSourcePath, "Src", "Tasks", task.name);
-                shell.mkdir('-p', targetPath);
-                shell.rm('-f', path.join(targetPath, '*.csproj'));
-                shell.rm('-f', path.join(targetPath, '*.md'));
-                // Path to UI contribution files
-                const uiPath = path.join(currentExtnRoot, "Src", "UIContribution");
-                // Statically link the required internal common modules.
-                var taskDeps;
-
-                if ((taskDeps = commonDeps[folderName])) {
-                    taskDeps.forEach(function (dep) {
-                        gutil.log('Linking ' + dep.module + ' into ' + folderName);
-                        var src = path.join(commonSrc, dep.module, "Src/");
-                        var dest = path.join(targetPath, dep.dest);
-                        shell.mkdir('-p', dest);
-                        fs.copy(src, dest, "*", function (err) {
-                            if (err) return console.error(err)
-                        });
-                    })
-                }
-
-                const externals = require('./externals.json');
-
-                // For building UI contribution using webpack
-                if (fs.existsSync(uiPath) && fs.statSync(uiPath).isDirectory()) {
-                    console.log(`⚒️  Building UI contribution for task: ${task.name}`);
-                    var originalDir = shell.pwd();
-                    util.cd(uiPath);
-                    util.run('npm ci');
-                    util.cd(originalDir);
-                }
-
-                if (Object.keys(task.execution).some(x => x.includes('Node'))) {
-                    if (externals['no-cache'].includes(task.name)) {
-                        console.log(`⚒️  Building Node task: ${task.name}`);
-
-                        const originalDir = shell.pwd();
-
-                        try {
-                            util.cd(taskDirPath);
-                            const npmrcPath = path.join(taskSourcePath, ".npmrc");
-                            const npmCmd = process.platform === 'win32' ? 'npm.cmd' : 'npm';
-                            const npmArgs = ['ci', '--userconfig', `"${npmrcPath}"`];
-                            if (util.isDebug()) {
-                                npmArgs.splice(1, 0, '--verbose');
-                                cp.execFileSync(npmCmd, npmArgs, { stdio: 'inherit', shell: true });
-                            } else {
-                                cp.execFileSync(npmCmd, npmArgs, { stdio: 'ignore', shell: true });
-                            }
-                            console.log(`\x1b[A\x1b[K✅ npm ci at ${taskDirPath} completed successfully.`);
-                        } catch (err) {
-                            console.log(`\x1b[A\x1b[K❌ npm ci at ${taskDirPath} failed. Error: ${err.message}`);
-                            process.exit(1);
-                        } finally {
-                            util.cd(originalDir);
+                    try {
+                        util.cd(taskDirPath);
+                        const npmrcPath = path.join(taskSourcePath, ".npmrc");
+                        const npmCmd = process.platform === 'win32' ? 'npm.cmd' : 'npm';
+                        const npmArgs = ['ci', '--userconfig', `"${npmrcPath}"`];
+                        if (util.isDebug()) {
+                            npmArgs.splice(1, 0, '--verbose');
+                            cp.execFileSync(npmCmd, npmArgs, { stdio: 'inherit', shell: true });
+                        } else {
+                            cp.execFileSync(npmCmd, npmArgs, { stdio: 'ignore', shell: true });
                         }
-                    } else {
-                        // Determine the azure-pipelines-task-lib version.
-                        var libVer = externals.npm['azure-pipelines-task-lib'];
-
-                        if (!libVer) {
-                            throw new Error('External azure-pipelines-task-lib not defined in externals.json.');
-                        }
-
-                        // Copy the lib from the cache.
-                        gutil.log('Linking azure-pipelines-task-lib ' + libVer);
-                        var copySource = path.join(_tempPath, 'npm', 'azure-pipelines-task-lib', libVer, 'node_modules', '**');
-                        var copyTarget = path.join(targetPath, 'node_modules');
-                        shell.mkdir('-p', copyTarget);
-                        gulp.src([copySource]).pipe(gulp.dest(copyTarget));
+                        console.log(`\x1b[A\x1b[K✅ npm ci at ${taskDirPath} completed successfully.`);
+                    } catch (err) {
+                        console.log(`\x1b[A\x1b[K❌ npm ci at ${taskDirPath} failed. Error: ${err.message}`);
+                        process.exit(1);
+                    } finally {
+                        util.cd(originalDir);
                     }
+                } else {
+                    // Determine the azure-pipelines-task-lib version.
+                    const libVer = externals.npm['azure-pipelines-task-lib'];
+
+                    if (!libVer) {
+                        throw new Error('External azure-pipelines-task-lib not defined in externals.json.');
+                    }
+
+                    // Copy the lib from the cache.
+                    gutil.log('Linking azure-pipelines-task-lib ' + libVer);
+                    const copySource = path.join(TEMP_PATH, 'npm', 'azure-pipelines-task-lib', libVer, 'node_modules', '**');
+                    const copyTarget = path.join(targetPath, 'node_modules');
+                    shell.mkdir('-p', copyTarget);
+                    gulp.src([copySource]).pipe(gulp.dest(copyTarget));
                 }
-                return;
-            })
-            .then(function() {
-                done();
-            })
-            .fail(function(err) {
-                done(err);
-            })
+            }
+        })
+        .then(function () {
+            done();
+        })
+        .catch(function (err) {
+            done(err);
         });
+    });
 }
 
 
