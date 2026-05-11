81919b6cd69deef2eb89a6fbf4b5750f296ab12c
nemanjarogic <nemanjarogic93@gmail.com>
2026-05-11 16:48:21 +0200

    Remove formatting changes

--- gulpfile.js
+++ gulpfile.js
@@ -80,28 +80,32 @@ function errorHandler(error) {
 
 var rootTsconfigPath = './base.tsconfig.json';
 
-gulp.task("clean", function () {
+gulp.task("clean", function() {
     return del([_buildRoot, _packageRoot, nugetPath, _taskModuleBuildRoot]);
 });
 
-gulp.task("compilePS", gulp.series("clean", function () {
+gulp.task("compilePS", gulp.series("clean", function() {
 
-    if (args.testAreaPath === undefined) {
+    if(args.testAreaPath === undefined )
+    {
         return gulp.src(sourcePaths, { base: "." }).pipe(gulp.dest(_buildRoot));
     }
-    else {
+    else
+    {
         var areaPathArgument = args.testAreaPath;
-        if (areaPathArgument.length > 0) {
+        if(areaPathArgument.length > 0 )
+        {
             console.log('Compiling updated modules - ' + areaPathArgument);
             var areaPaths = areaPathArgument.trim().split(',');
             var filter = [];
             for (var n = 0; n < areaPaths.length; n++) {
                 filter.push(ExtensionFolder + '/' + areaPaths[n] + '/**/*')
-            }
+                }
 
             return gulp.src(filter, { base: "." }).pipe(gulp.dest(_buildRoot));
         }
-        else {
+        else
+        {
             console.log('No module is updated with given change-set');
             // Create a _build/Extensions folder which will be empty
             return gulp.src(ExtensionFolder, { base: "." }).pipe(gulp.dest(_buildRoot));
@@ -109,11 +113,11 @@ gulp.task("compilePS", gulp.series("clean", function () {
     }
 }));
 
-gulp.task("TaskModuleBuild", gulp.series("clean", function () {
-    gulp.src(taskModulesSourcePath, { base: "." }).pipe(gulp.dest(_buildRoot));
+gulp.task("TaskModuleBuild", gulp.series("clean", function() {
+    gulp.src(taskModulesSourcePath, { base: "."}).pipe(gulp.dest(_buildRoot));
 }));
 
-gulp.task("clean:TaskModuleTest", function (cb) {
+gulp.task("clean:TaskModuleTest", function(cb) {
     return del([TaskModulesTestRoot], cb);
 });
 
@@ -146,7 +150,7 @@ gulp.task('copy:TaskModuleTest', gulp.series('compile:TaskModuleTest', function 
         .pipe(gulp.dest(TaskModulesTestRoot));
 }));
 
-gulp.task("TaskModuleTest", gulp.series('copy:TaskModuleTest', function () {
+gulp.task("TaskModuleTest", gulp.series('copy:TaskModuleTest', function() {
     process.env['TASK_TEST_TEMP'] = TaskModulesTestTemp;
     shell.rm('-rf', TaskModulesTestTemp);
     shell.mkdir('-p', TaskModulesTestTemp);
@@ -165,15 +169,15 @@ gulp.task('prepublish:TaskModulePublish', function (done) {
 gulp.task('TaskModulePublish', gulp.series('prepublish:TaskModulePublish', function (done) {
     var powershellModulesDirectory = path.join(_taskModuleBuildRoot, 'powershell', '**/*');
 
-    if (options.outputDir) {
+    if(options.outputDir){
         var outputModulesDirectory = path.join(options.outputDir, 'ps_modules');
         shell.mkdir('-p', outputModulesDirectory);
         gulp.src(powershellModulesDirectory).pipe(gulp.dest(outputModulesDirectory));
     }
 }));
 
-gulp.task("compileNode", gulp.series("compilePS", function (cb) {
-    try {
+gulp.task("compileNode", gulp.series("compilePS", function(cb){
+     try {
         // Cache all externals in the download directory.
         var allExternalsJson = shell.find(path.join(__dirname, 'Extensions'))
             .filter(function (file) {
@@ -207,8 +211,8 @@ gulp.task("compileNode", gulp.series("compilePS", function (cb) {
 
                     var packageSource = cacheNuGetV2Package(packageRepository, package, packageVersion);
 
-                    var relativeExternalsPath = path.dirname(externalsJson).replace(new RegExp('/', 'g'), '\\').replace(path.join(__dirname), '');
-                    if (relativeExternalsPath.startsWith('\\')) {
+                    var relativeExternalsPath = path.dirname(externalsJson).replace(new RegExp('/','g'),'\\').replace(path.join(__dirname),'');
+                    if(relativeExternalsPath.startsWith('\\')) {
                         relativeExternalsPath = relativeExternalsPath.substring(1);
                     }
                     var destPath = path.join(_buildRoot, relativeExternalsPath);
@@ -230,18 +234,18 @@ gulp.task("compileNode", gulp.series("compilePS", function (cb) {
             }
 
             // check of task modules
-            if (externals.taskModule) {
+            if(externals.taskModule) {
                 var taskModules = Object.keys(externals.taskModule);
                 taskModules.forEach(function (moduleIndex) {
-                    var module = externals.taskModule[moduleIndex];
-                    var srcPath = path.join("TaskModules", module['type'], module['name']);
-                    var relativeExternalsPath = path.dirname(externalsJson).replace(new RegExp('/', 'g'), '\\').replace(path.join(__dirname), '');
-                    if (relativeExternalsPath.startsWith('\\')) {
-                        relativeExternalsPath = relativeExternalsPath.substring(1);
-                    }
-                    var destPath = path.join(_buildRoot, relativeExternalsPath, module['dest']);
-                    shell.mkdir('-p', destPath);
-                    shell.cp('-R', srcPath, destPath);
+                      var module = externals.taskModule[moduleIndex];
+                      var srcPath = path.join("TaskModules", module['type'], module['name']);
+                      var relativeExternalsPath = path.dirname(externalsJson).replace(new RegExp('/','g'),'\\').replace(path.join(__dirname),'');
+                      if(relativeExternalsPath.startsWith('\\')) {
+                         relativeExternalsPath = relativeExternalsPath.substring(1);
+                      }
+                      var destPath = path.join(_buildRoot, relativeExternalsPath, module['dest']);
+                      shell.mkdir('-p', destPath);
+                      shell.cp('-R', srcPath, destPath);
                 });
             }
         });
@@ -253,7 +257,7 @@ gulp.task("compileNode", gulp.series("compilePS", function (cb) {
     }
 
     // Compile UIExtensions
-    fs.readdirSync(path.join(__dirname, 'Extensions/')).filter(function (file) {
+    fs.readdirSync( path.join(__dirname, 'Extensions/')).filter(function (file) {
         return fs.statSync(path.join(_extnBuildRoot, file)).isDirectory() && file != "Common";
     }).forEach(compileUIExtensions);
 
@@ -668,20 +672,20 @@ function runNpmInstall(packagePath) {
 }
 
 function compileUIExtensions(extensionRoot) {
-    var uiExtensionsPath = path.join(_buildRoot, "Extensions", extensionRoot, 'Src', 'UIExtensions');
-    var tsconfigPath = path.join(uiExtensionsPath, "tsconfig.json");
+    var uiExtensionsPath = path.join(_buildRoot,"Extensions", extensionRoot, 'Src', 'UIExtensions');
+    var tsconfigPath = path.join(uiExtensionsPath,"tsconfig.json");
     if (fs.existsSync(tsconfigPath)) {
         var projLocal = gts.createProject(tsconfigPath, { typescript: typescript });
         var tsLocal = gts(projLocal);
         var uiFilePath = path.join(uiExtensionsPath, '**/**/*.ts');
         return gulp.src([uiFilePath])
-            .pipe(tsLocal)
-            .on('error', errorHandler)
-            .pipe(gulp.dest(uiExtensionsPath));
+        .pipe(tsLocal)
+        .on('error', errorHandler)
+        .pipe(gulp.dest(uiExtensionsPath));
     };
 }
 
-gulp.task("updateTestIds", function (cb) {
+gulp.task("updateTestIds", function(cb) {
     if (args.test) {
         var buildExtensionsRoot = path.join(_buildRoot, 'Extensions');
         if (fs.existsSync(buildExtensionsRoot)) {
@@ -760,7 +764,7 @@ gulp.task("updateTestIds", function (cb) {
     cb();
 });
 
-gulp.task("syncVersions", function (cb) {
+gulp.task("syncVersions", function(cb) {
     var input = args.syncVersions || args.syncversions;
     if (!input) {
         cb();
@@ -864,7 +868,7 @@ gulp.task("syncVersions", function (cb) {
 
 gulp.task("build", gulp.series("syncVersions", "compileNode", "updateTestIds"));
 
-gulp.task("handoff", function (cb) {
+gulp.task("handoff", function(cb) {
     handoff(cb);
 });
 
@@ -881,28 +885,28 @@ gulp.task('compileTests', function () {
 
     return gulp.src([testsPath, commonFiles, 'definitions/*.d.ts'])
         .pipe(proj())
-        .pipe(gulp.dest(_testRoot + "\\Extensions"))
+        .pipe(gulp.dest(_testRoot+"\\Extensions"))
         .on('error', errorHandler);
 });
 
 gulp.task('testLib', gulp.series('compileTests', function () {
     return gulp.src(['Extensions/Common/lib/**/*'])
-        .pipe(gulp.dest(path.join(_testRoot, 'Extensions/Common/lib/')));
+        .pipe(gulp.dest(path.join(_testRoot,'Extensions/Common/lib/')));
 }));
 
 gulp.task('copyTestData', gulp.series('compileTests', function () {
     return gulp.src(['Extensions/**/Tests/**/data/**'], { dot: true })
-        .pipe(gulp.dest(_testRoot + "\\Extensions"));
+        .pipe(gulp.dest(_testRoot+"\\Extensions"));
 }));
 
 gulp.task('tstests', gulp.series('compileTests', function () {
     return gulp.src(['Extensions/**/Tests/**/*.ts', 'Extensions/**/Tests/**/*.json', 'Extensions/**/Tests/**/*.js'])
-        .pipe(gulp.dest(_testRoot + "\\Extensions"));
+        .pipe(gulp.dest(_testRoot+"\\Extensions"));
 }));
 
 gulp.task('ps1tests', gulp.series('compileTests', function () {
     return gulp.src(['Extensions/**/Tests/**/*.ps1', 'Extensions/**/Tests/**/*.json'])
-        .pipe(gulp.dest(_testRoot + "\\Extensions"));
+        .pipe(gulp.dest(_testRoot+"\\Extensions"));
 }));
 
 gulp.task('testLib_NodeModules', gulp.series('testLib', function () {
@@ -912,12 +916,12 @@ gulp.task('testLib_NodeModules', gulp.series('testLib', function () {
 
 gulp.task('testResources', gulp.parallel('testLib_NodeModules', 'ps1tests', 'tstests', 'copyTestData'));
 
-gulp.task("test", gulp.series("testResources", function () {
-    process.env['TASK_TEST_TEMP'] = path.join(__dirname, _testTemp);
+gulp.task("test", gulp.series("testResources", function(){
+    process.env['TASK_TEST_TEMP'] =path.join(__dirname, _testTemp);
     shell.rm('-rf', _testTemp);
     shell.mkdir('-p', _testTemp);
 
-    if (options.suite.indexOf("ArtifactEngine") >= 0 && options.e2e) {
+    if (options.suite.indexOf("ArtifactEngine") >= 0  && options.e2e) {
         var suitePath = path.join(_testRoot, "Extensions/" + options.suite + "/**/*e2e.js");
         console.log(suitePath);
         var tfBuild = ('' + process.env['TF_BUILD']).toLowerCase() == 'true'
@@ -925,7 +929,7 @@ gulp.task("test", gulp.series("testResources", function () {
             .pipe(mocha({ reporter: 'spec', ui: 'bdd', useColors: !tfBuild }));
     }
 
-    if (options.suite.indexOf("ArtifactEngine") >= 0 && options.perf) {
+    if (options.suite.indexOf("ArtifactEngine") >= 0  && options.perf) {
         var suitePath = path.join(_testRoot, "Extensions/" + options.suite + "/**/*perf.js");
         console.log(suitePath);
         var tfBuild = ('' + process.env['TF_BUILD']).toLowerCase() == 'true'
@@ -933,13 +937,13 @@ gulp.task("test", gulp.series("testResources", function () {
             .pipe(mocha({ reporter: 'spec', ui: 'bdd', useColors: !tfBuild }));
     }
 
-    var suitePath = path.join(_testRoot, "Extensions/" + options.suite + "/Tests/Tasks", options.suite + '/_suite.js');
+    var suitePath = path.join(_testRoot,"Extensions/" + options.suite + "/Tests/Tasks", options.suite + '/_suite.js');
     console.log(suitePath);
     var suitePath2 = path.join(_testRoot, "Extensions/" + options.suite + "/**/*Tests.js");
     console.log(suitePath2);
     var tfBuild = ('' + process.env['TF_BUILD']).toLowerCase() == 'true'
-    var ignorePath = "!" + path.join(_testRoot, "Extensions", "/**/UIContribution{,/**}");
-    return gulp.src([suitePath, suitePath2, ignorePath], { allowEmpty: true })
+    var ignorePath = "!" + path.join(_testRoot, "Extensions",  "/**/UIContribution{,/**}");
+    return gulp.src([ suitePath, suitePath2, ignorePath ], { allowEmpty: true })
         .pipe(mocha({ reporter: 'spec', ui: 'bdd', useColors: !tfBuild }));
 }));
 
@@ -947,14 +951,14 @@ gulp.task("test", gulp.series("testResources", function () {
 // Package//-----------------------------------------------------------------------------------------------------------------
 
 var publisherName = null;
-gulp.task("package", function (cb) {
+gulp.task("package", function(cb) {
     if (args.publisher) {
         publisherName = args.publisher;
     }
 
     // use gulp package --extension=<Extension_Name> to package an individual package
     if (args.extension) {
-        createVsixPackage(args.extension); return;
+        createVsixPackage(args.extension);        return;
     }
 
     fs.readdirSync(_extnBuildRoot).filter(function (file) {
@@ -963,9 +967,9 @@ gulp.task("package", function (cb) {
     cb();
 });
 
-gulp.task('nuget-download', function (done) {
+gulp.task('nuget-download', function(done) {
     console.log("> Checking for nuget.exe");
-    if (fs.existsSync('nuget.exe')) {
+    if(fs.existsSync('nuget.exe')) {
         return done();
     }
     console.log("> Downloading nuget.exe");
@@ -973,7 +977,7 @@ gulp.task('nuget-download', function (done) {
         .pipe(fs.createWriteStream('nuget.exe'));
 });
 
-gulp.task("package_nuget", gulp.series('nuget-download', function () {
+gulp.task("package_nuget", gulp.series('nuget-download', function() {
 
     // nuspec
     var version = options.version;
@@ -987,12 +991,12 @@ gulp.task("package_nuget", gulp.series('nuget-download', function () {
         process.exit(1);
     }
 
-    if (!options.extension) {
+    if(!options.extension) {
         console.error('ERROR: supply extension name with --extension');
         process.exit(1);
     }
 
-    if (!fs.existsSync("_package\\" + options.extension)) {
+    if(!fs.existsSync("_package\\"+options.extension)) {
         console.error('ERROR: mentioned extension does not exist');
         process.exit(1);
     }
@@ -1002,9 +1006,9 @@ gulp.task("package_nuget", gulp.series('nuget-download', function () {
     var extensionPath = path.join("_package", options.extension);
 
     shell.rm("-rf", nugetPath);
-    var contentsPath = path.join(nugetPath, 'pack-source', 'contents');
+    var contentsPath = path.join(nugetPath,'pack-source', 'contents');
     shell.mkdir("-p", contentsPath);
-    shell.cp(path.join(extensionPath, "*"), contentsPath);
+    shell.cp(path.join(extensionPath,"*"), contentsPath);
 
     // nuspec
     var pkgName = 'Mseng.MS.TF.RM.Extensions';
@@ -1051,43 +1055,43 @@ gulp.task("package_nuget", gulp.series('nuget-download', function () {
 }));
 
 
-gulp.task("locCommon", function () {
+gulp.task("locCommon",function(){
     return gulp.src(path.join(__dirname, 'Extensions/Common/**/module.json'))
-        .pipe(pkgm.LocCommon());
+             .pipe(pkgm.LocCommon());
 });
 
-var copyCommonModules = function (extensionName) {
+var copyCommonModules = function(extensionName) {
     var commonDeps = require('./common.json');
     var commonSrc = path.join(__dirname, 'Extensions/Common');
-    var currentExtnRoot = path.join(__dirname, "_build/Extensions", extensionName);
+    var currentExtnRoot = path.join(__dirname, "_build/Extensions" ,extensionName);
     const extensionSourcePath = path.join(__dirname, "Extensions", extensionName);
     return gulp.src(path.join(currentExtnRoot, '**/task.json'))
         .pipe(pkgm.copyCommonModules(currentExtnRoot, commonDeps, commonSrc, extensionSourcePath));
 }
 
-var createVsixPackage = function (extensionName) {
+var createVsixPackage = function(extensionName) {
     var extnOutputPath = path.join(_packageRoot, extensionName);
     var extnManifestPath = path.join(_extnBuildRoot, extensionName, "Src");
 
-    if (fs.existsSync(extnManifestPath)) {
-        del(extnOutputPath);
-        if (publisherName) {
-            var manifest = JSON.parse(fs.readFileSync(path.join(extnManifestPath, "vss-extension.json")));
-            manifest.publisher = publisherName;
-            fs.writeFileSync(path.join(extnManifestPath, "vss-extension.json"), JSON.stringify(manifest));
-        }
-        shell.mkdir("-p", extnOutputPath);
-        var packagingCmd = "tfx extension create --manifest-globs vss-extension.json --root " + extnManifestPath + " --output-path " + extnOutputPath;
-        executeCommand(packagingCmd, function () { });
+    if(fs.existsSync(extnManifestPath)) {
+      del(extnOutputPath);
+      if (publisherName){
+          var manifest = JSON.parse(fs.readFileSync(path.join(extnManifestPath,"vss-extension.json")));
+          manifest.publisher = publisherName;
+          fs.writeFileSync(path.join(extnManifestPath,"vss-extension.json"), JSON.stringify(manifest));
+      }
+      shell.mkdir("-p", extnOutputPath);
+      var packagingCmd = "tfx extension create --manifest-globs vss-extension.json --root " + extnManifestPath + " --output-path " + extnOutputPath;
+      executeCommand(packagingCmd, function() {});
     }
 }
 
-var executeCommand = function (cmd, callback) {
-    shell.exec(cmd, { silent: true }, function (code, output) {
+var executeCommand = function(cmd, callback) {
+    shell.exec(cmd, {silent: true}, function(code, output) {
         if (code != 0) {
             console.error("command failed: " + cmd + "\nManually execute to debug");
         } else {
-            callback();
+           callback();
         }
     });
 }
