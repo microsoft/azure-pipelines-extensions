ec656e0be14a2e761b085ba22ade5a43b1df3f98
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2026-09-04 14:30:37 +0200

    Refactor auth parameter handling: ensure safe access to parameters in getAuthParameter function

--- Extensions/ExternalTfs/Tests/Tasks/DownloadArtifactsTfsGit/failMissingAuthParameterObject.ts
+++ Extensions/ExternalTfs/Tests/Tasks/DownloadArtifactsTfsGit/failMissingAuthParameterObject.ts
@@ -20,8 +20,7 @@ tr.setInput('downloadPath', DOWNLOAD_PATH);
 
 process.env['ENDPOINT_URL_' + TFS_CONNECTION_ID] = 'https://tfs.example.local/DefaultCollection';
 process.env['ENDPOINT_AUTH_' + TFS_CONNECTION_ID] = JSON.stringify({
-    scheme: 'UsernamePassword',
-    parameters: {}
+    scheme: 'UsernamePassword'
 });
 process.env['ENDPOINT_AUTH_SCHEME_' + TFS_CONNECTION_ID] = 'UsernamePassword';
 
